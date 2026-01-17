d789755638bfb0ca2671f49fb7dd6d2f01f7b885
hugobardales <vhbardalesp@gmail.com>
2026-01-17 10:46:52 -0500

    Initial Bootstrap structure for DataBridge_crm_fullstack proyect

---
+++ databridge_crm_backend/src/main/java/com/databridge_crm_backend/DatabridgeCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.databridge_crm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabridgeCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabridgeCrmBackendApplication.class, args);
+    }
+
+}
---
+++ databridge_crm_backend/src/test/java/com/databridge_crm_backend/DatabridgeCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databridge_crm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabridgeCrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
