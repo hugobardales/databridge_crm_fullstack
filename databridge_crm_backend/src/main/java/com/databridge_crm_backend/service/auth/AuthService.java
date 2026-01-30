710086907582af2899ac9945460c4b4296983387
hugobardales <vhbardalesp@gmail.com>
2026-01-29 20:40:18 -0500

    feat(service/auth): Updating AuthService class to adding two inyect dependencies as authenticationManager and jwtUtils to handle the bussines logic behind register and logic.

--- databridge_crm_backend/src/main/java/com/databridge_crm_backend/service/auth/AuthService.java
+++ databridge_crm_backend/src/main/java/com/databridge_crm_backend/service/auth/AuthService.java
@@ -3,10 +3,17 @@ package com.databridge_crm_backend.service.auth;
 import com.databridge_crm_backend.domain.model.Role;
 import com.databridge_crm_backend.domain.model.User;
 import com.databridge_crm_backend.domain.repository.UserRepository;
+import com.databridge_crm_backend.dto.auth.AuthResponse;
+import com.databridge_crm_backend.dto.auth.LoginRequest;
 import com.databridge_crm_backend.dto.auth.RegisterRequest;
+import com.databridge_crm_backend.security.JwtUtils;
 import jakarta.transaction.Transactional;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import static org.springframework.security.core.userdetails.User.withUsername;
+import com.databridge_crm_backend.dto.auth.AuthResponse;
 
 @Service
 public class AuthService {
@@ -14,9 +21,15 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
     }
 
     @Transactional
@@ -28,7 +41,26 @@ public class AuthService {
         User user = new User(
                 request.email(),
                 passwordEncoder.encode(request.password()),
-                Role.USER);
+                Role.USER); // habilitado solo para registros como user
         userRepository.save(user);
+    }
+
+    public AuthResponse login(LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.email(),
+                        loginRequest.password()));
+
+        User user = userRepository.findByEmail(loginRequest.email())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
+
+        var userDetails = withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .build();
+
+        String jwt = jwtUtils.generateToken(userDetails);
+        return new AuthResponse(jwt);
+
     }
 }
