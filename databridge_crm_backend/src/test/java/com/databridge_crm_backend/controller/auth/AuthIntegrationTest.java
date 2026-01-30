c77f492dac23652f262144f4055dd64a28f80378
hugobardales <vhbardalesp@gmail.com>
2026-01-29 23:32:19 -0500

    fix: all test passed troubleshooting context errors in integration tests.

--- databridge_crm_backend/src/test/java/com/databridge_crm_backend/controller/auth/AuthIntegrationTest.java
+++ databridge_crm_backend/src/test/java/com/databridge_crm_backend/controller/auth/AuthIntegrationTest.java
@@ -2,6 +2,7 @@ package com.databridge_crm_backend.controller.auth;
 
 import com.databridge_crm_backend.domain.model.Role;
 import com.databridge_crm_backend.domain.repository.UserRepository;
+import com.databridge_crm_backend.dto.auth.LoginRequest;
 import com.databridge_crm_backend.dto.auth.RegisterRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -60,5 +62,28 @@ class AuthIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    void houldLoginSuccessfully() throws Exception {
+        // Arrange
+        String email = "login@example.com";
+        String password = "password123";
+        RegisterRequest registerRequest = new RegisterRequest(email, password);
+
+        // We perform register manually (or via service) to ensure user exists
+        mockMvc.perform(post("/api/v1/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isCreated());
+
+        // Act
+        LoginRequest loginRequest = new LoginRequest(email, password);
+
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists());
     }
 }
